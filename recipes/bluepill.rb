c71bb5057edd63436b598a92ea360524178ef5bd
double-z <zackzondlo@gmail.com>
2013-02-18 21:56:48 -0500

    bluepill runit tweaks

--- recipes/bluepill.rb
+++ recipes/bluepill.rb
@@ -1,15 +1,14 @@
 include_recipe "bluepill"
 include_recipe "runit"
+include_recipe "monit"
 
-bluepill_service "monit"
-
-service "monit" do
-  supports :status => true, :restart => true, :start => true, :stop => true, :reload => true
-  reload_command "#{node['runit']['sv_bin']} hup #{node['runit']['service_dir']}/monit"
-  start_command "#{node['runit']['sv_bin']} start #{node['runit']['service_dir']}/monit"
-  stop_command "#{node['runit']['sv_bin']} stop #{node['runit']['service_dir']}/monit"
-  restart_command "#{node['runit']['sv_bin']} restart #{node['runit']['service_dir']}/monit"
-	action :nothing
+runit_service "monit" do
+  default_logger true
 end
 
-include_recipe "monit"
+bluepill_service "monit" do
+  action [:enable, :load, :start]
+end
+
+
+
